f9474b5d87a7db0c622fbe1bf08e55fc059ea8fa
Shira Nahmanu <shiralior.nahmany@post.runi.ac.il>
2025-11-05 13:56:33 +0200

    Completed assignment

--- FVCalc.java
+++ FVCalc.java
@@ -4,8 +4,8 @@ public class FVCalc {
 		int invest = Integer.parseInt(args[0]);
 		double p = Double.parseDouble(args[1]);
 		int yr = Integer.parseInt(args[2]);
-		double calc = invest * Math.pow(1 + (p/100.0) , yr);
-		System.err.println("After " + yr + " years, " + invest + "$ saved at " + (int)p + "% will yield " 
-		+ (int)calc + "$");
+		double calc = invest * Math.pow((1 + (p / 100.0)) , yr);
+		System.err.println("After " + yr + " years, $" + invest + " saved at " + p + "% will yield $" 
+		+ (int)calc);
 	}
 }
